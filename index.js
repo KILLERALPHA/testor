d916132eda1fddb0baae4756af8de9a4cf40f8de
RZB RAZOR <65178209+KILLERALPHA@users.noreply.github.com>
2023-10-22 12:10:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express');
-const app = express();
+const crypto = require('crypto');
 
-app.get('/', (req, res) => {
-  const currentDate = new Date();
-  const port = process.env.PORT;
-  res.send('Current Date and Time: ' + currentDate + ' PORT : '+port);
-});
+// Encrypt a message using AES encryption
+function encryptText(text, secretKey) {
+  const cipher = crypto.createCipher('aes-256-cbc', secretKey);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+}
 
-module.exports = app;
+// Decrypt the message
+function decryptText(encryptedText, secretKey) {
+  const decipher = crypto.createDecipher('aes-256-cbc', secretKey);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
+
+module.exports = (req, res) => {
+  const queryData = req.query.data || ''; // Get the 'data' parameter from the URL
+  const secretKey = 'yourSecretKey'; // Replace with your secret key
+
+  const encryptedText = encryptText(queryData, secretKey);
+  const decryptedText = decryptText(encryptedText, secretKey);
+
+  // Send both encrypted and decrypted text in the response
+  res.status(200).send(`Encrypted Text: ${encryptedText}\nDecrypted Text: ${decryptedText}`);
+};
