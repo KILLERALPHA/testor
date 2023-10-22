5ff3cd8f19a6d6249809f6d0fe78c73b929e2322
RZB RAZOR <65178209+KILLERALPHA@users.noreply.github.com>
2023-10-22 12:04:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-// index.js
-/*
-const express = require('express')
-
-const app = express()
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳 AHHH ')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app*/
-//console.log("AHH BABY");
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   const currentDate = new Date();
-  res.send('Current Date and Time: ' + currentDate);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Server is running on port ' + port);
+  const port = process.env.PORT;
+  res.send('Current Date and Time: ' + currentDate + ' PORT : '+port);
 });
 
 module.exports = app;
